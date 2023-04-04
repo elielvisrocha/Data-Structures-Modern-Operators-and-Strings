fb9933d8a84eff2907cb9b4446d2d5bac8cd4314
elielvisrocha <rochaelielvis@gmail.com>
2023-04-03 22:55:02 -0300

    for of

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 'use strict';
 
-const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// let hours = {
+//         thu: {
+//             open: 12,
+//             close: 22,
+//         },
+//         fri: {
+//             open: 11,
+//             close: 23,
+//         },
+//         sat: {
+//             open: 0, // Open 24 hours
+//             close: 24,
+//         }
+// }
 
-    order: function (starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    },
+// const restaurant = {
+//     name: 'Classico Italiano',
+//     location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//     hours,
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
-};
+//     order(starterIndex, mainIndex) {
+//         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//     },
+//     hours
+// };
 
 
 
 
-const rest1 = {
-    name: 'DiMaria Ferdinando',
-    numGuests: 0
-    // numGuests: 192
-};
+// const rest1 = {
+//     name: 'DiMaria Ferdinando',
+//     numGuests: 0
+//     // numGuests: 192
+// };
 
-const rest2 = {
-    name: 'Lazzana tomato',
-    owner: 'Nicolai Domingos'
-};
+// const rest2 = {
+//     name: 'Lazzana tomato',
+//     owner: 'Nicolai Domingos'
+// };
 
-// rest1.owner = rest1.name && 'Hey!';
-// rest2.owner = rest2.owner && 'Hey!';
+// // rest1.owner = rest1.name && 'Hey!';
+// // rest2.owner = rest2.owner && 'Hey!';
 
-rest1.name &&= 'Hey!';
-rest2.owner &&= 'Hey!';
+// rest1.name &&= 'Hey!';
+// rest2.owner &&= 'Hey!';
 
-console.log(rest1.name);
-console.log(rest2.owner);
+// console.log(rest1.name);
+// console.log(rest2.owner);
 
 
 // OR ASSIGMENT OPERATOR
@@ -241,3 +243,36 @@ console.log(rest2.owner);
     // Data needed for a later exercise
     // const flights =
     //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+
+
+
+
+
+
+
+
+
+// FOR OF LOOPS
+
+
+
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+// for(let items of menu) console.log(items);
+
+
+// for (let itemsWithIndex of menu.entries()) {
+//     console.log(itemsWithIndex);
+// }
+
+// for (let [i, el] of menu.entries()) {
+//     console.log(`${i + 1}: ${el}`);
+// }
+
+
+
+
+
+
+
